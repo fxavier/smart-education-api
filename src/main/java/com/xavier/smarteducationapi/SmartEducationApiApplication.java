f664ed1d0736ed7b6e10d2ceb4d4f4ba058a3839
Xavier Francisco <xavierfrancisco353@gmail.com>
2025-09-15 13:16:35 +0100

    first commit

---
+++ src/main/java/com/xavier/smarteducationapi/SmartEducationApiApplication.java
@@ -0,0 +1,13 @@
+package com.xavier.smarteducationapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartEducationApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartEducationApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/xavier/smarteducationapi/SmartEducationApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xavier.smarteducationapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartEducationApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
